fd278a5eb542166f09614e7b251790f95b5619b9
osorio <germanosorioj@gmail.com>
2021-10-04 21:15:28 -0500

    Se crea la pagina index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,9 @@
-import CardRazas from './components/CardRazasPerros'; 
-import labrador from './media/Labrador.jpg';
-import husky from './media/Husky Siberiano.png';
-import logoperro from './media/logo-perro.jpg';
 import './App.css';
-
+import Index from "./pages/index";
 function App() {
   return (
     <div className="App">
-      <header>
-        <ul className="navbar">
-          <li>
-            <img src={logoperro} alt="logo" className="logo" />
-          </li>
-          <li> <button className="button mainButton">Nuevo Post</button> </li>
-          <li>
-            <div className="buscar">
-              <input placeholder="Buscar una raza" />
-              <i className="fas fa-search button icono-busqueda"></i>
-            </div>
-          </li>
-          <li><button className="button secundaryButton">Login</button></li>
-          <li><button className="button mainButton">Registro</button> </li>
-        </ul>
-      </header>
-      <main>
-        <section>
-          <h1>Razas de Perros</h1>
-          <ul className="card-container">
-              <CardRazas nombreRaza="Labrador" imagen={labrador}/>
-              <CardRazas nombreRaza="Husky Siberiano" imagen={husky}/>
-              <CardRazas nombreRaza="Labrador" imagen={labrador}/>
-              <CardRazas nombreRaza="Husky Siberiano" imagen={husky}/>
-          </ul>
-        </section>
-        <section>
-
-        </section>
-        <footer>
-
-        </footer>
-      </main>
-      <footer>
-      </footer>
+      <Index/>
     </div>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,46 @@
-import CardRazas from "components/CardRazasPerros";
-function Index( ){
-    return(
-        <div>Hola, soy el index</div>
+import labrador from '../media/Labrador.jpg';
+import husky from '../media/Husky Siberiano.png';
+import logoperro from '../media/logo-perro.jpg';
+import CardRazas from "../components/CardRazasPerros";
+function Index() {
+    return (
+        <div>
+            <header>
+                <ul className="navbar">
+                    <li>
+                        <img src={logoperro} alt="logo" className="logo" />
+                    </li>
+                    <li> <button className="button mainButton">Nuevo Post</button> </li>
+                    <li>
+                        <div className="buscar">
+                            <input placeholder="Buscar una raza" />
+                            <i className="fas fa-search button icono-busqueda"></i>
+                        </div>
+                    </li>
+                    <li><button className="button secundaryButton">Login</button></li>
+                    <li><button className="button mainButton">Registro</button> </li>
+                </ul>
+            </header>
+            <main>
+                <section>
+                    <h1>Razas de Perros</h1>
+                    <ul className="card-container">
+                        <CardRazas nombreRaza="Labrador" imagen={labrador} />
+                        <CardRazas nombreRaza="Husky Siberiano" imagen={husky} />
+                        <CardRazas nombreRaza="Labrador" imagen={labrador} />
+                        <CardRazas nombreRaza="Husky Siberiano" imagen={husky} />
+                    </ul>
+                </section>
+                <section>
+
+                </section>
+                <footer>
+
+                </footer>
+            </main>
+            <footer>
+            </footer>
+        </div>
     )
 
 }
